737629837a006fbbb84f564bbc0b1e0fc4a9af0a
9080364890z <mshaikhkarim22@gmail.com>
2024-10-11 00:30:18 -0700

    add code

---
+++ arithmeticopr.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+
+    printf("enter value of a=\n");
+    scanf("%d",&a);
+    
+    printf("enter value of b=\n");
+    scanf("%d",&b);
+
+        printf(" addition =%d\n",a+b);
+        printf("multiplication=%d\n",a*b);
+        printf("division=%d\n",a/b);
+        printf("substraction=%d\n",a-b);
+        
+        return 0;
+
+}
